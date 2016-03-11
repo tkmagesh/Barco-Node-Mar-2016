63103a68494e87a9ec0e3d4afefdbabdfbfc6a13
tkmagesh <tkmagesh77@gmail.com>
2016-03-11 13:27:00 +0530

    complete checkin

--- Day-01/04-webserver/program.js
+++ Day-01/04-webserver/program.js
@@ -3,18 +3,28 @@ var http = require('http'),
 	url = require('url'),
 	path = require('path');
 
+var staticResourceExtns = ['.html','.js','.xml','.json','.png','.jpg','.ico'];
+
+function isStatic(resource){
+	return staticResourceExtns.indexOf(path.extname(resource)) !== -1;
+}
 var server = http.createServer(function(req, res){
 	console.log('A new connection is established for - ', req.url);
 	req.url = url.parse(req.url === '/' ? '/index.html' : req.url, true);
 	console.log(req.url);
 	var resource = path.join(__dirname, req.url.pathname);
-	if (!fs.existsSync(resource)){
-		res.statusCode = 404;
+	if (isStatic(resource)){
+		if (!fs.existsSync(resource)){
+			res.statusCode = 404;
+			res.end();
+			return;
+		}
+		var stream = fs.createReadStream(resource, {encoding : 'utf8'});
+		stream.pipe(res);
+	} else {
+		res.write('Dynamic resource - coming soon!!');
 		res.end();
-		return;
 	}
-	var stream = fs.createReadStream(resource, {encoding : 'utf8'});
-	stream.pipe(res);
 });
 
 server.listen(8080);
